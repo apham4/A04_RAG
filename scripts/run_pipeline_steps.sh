6153ba8f170807cf4115f7a1acc612a68b316645
Anh Pham <phamquocanh2580@gmail.com>
2025-06-10 18:49:04 -0400

    What did I set up here I forgot

--- scripts/run_pipeline_steps.sh
+++ scripts/run_pipeline_steps.sh
@@ -1,36 +1,36 @@
 # turn off chromadb sending stats back
 export ANONYMIZED_TELEMETRY=False
 
-BASEDIR="C:\Users\phamq\OneDrive\Desktop\school stuff\691\A04\RAG_project\code"
+BASEDIR="C:\Users\phamq\OneDrive\Desktop\school stuff\691\A04\RAG_project\A04_RAG"
 
 
 # ----------------------------------
 #  Step 01: Ingest: parse pdf or text files into cleaned text
 # ----------------------------------
-python "$BASEDIR/main.py" step01_ingest --input_filename  "P1 - Group 1 - Proposal.pdf"
-# python "$BASEDIR/main.py" step01_ingest --input_filename all
+# python "$BASEDIR/main.py" step01_ingest --input_filename  "P1 - Group 1 - Proposal.pdf"
+#python "$BASEDIR/main.py" step01_ingest --input_filename all
 
 # ----------------------------------
 #  Step 02: Generate Embeddings from the cleaned text files
 # ----------------------------------
 # python "$BASEDIR/main.py" step02_generate_embeddings --input_filename Zhang_et_al_2024_LLMs_cleaned.txt
-# python "$BASEDIR/main.py" step02_generate_embeddings --input_filename all
+#python "$BASEDIR/main.py" step02_generate_embeddings --input_filename all
 
 # ----------------------------------
 #  Step 03: Store the cleaned text and embeddings in a vector db
 # ----------------------------------
 # python "$BASEDIR/main.py" step03_store_vectors  --input_filename Zhang_et_al_2024_LLMs_cleaned.txt
-# python "$BASEDIR/main.py" step03_store_vectors --input_filename  all
+#python "$BASEDIR/main.py" step03_store_vectors --input_filename  all
 
 # ----------------------------------
 #  Step 04: Retrieve chunks of text and similarity scores
 # ----------------------------------
-# python "$BASEDIR/main.py" step04_retrieve_chunks --query_args "regional development in japan"
+#python "$BASEDIR/main.py" step04_retrieve_chunks --query_args "allergy and asthma"
 
 # ----------------------------------
 #  Step 05: Run LLM Queries with and without RAG
 #  	If the parameter "--use_rag" is not provided, RAG is not performed
 # ----------------------------------
-# QUERY="Discuss trends in regional development in Japan"
+QUERY="Discuss the most up to date guidelines for asthma management"
 # python "$BASEDIR/main.py" step05_generate_response  --query_args "$QUERY"
-# python "$BASEDIR/main.py" step05_generate_response  --query_args "$QUERY"  --use_rag
+python "$BASEDIR/main.py" step05_generate_response  --query_args "$QUERY"  --use_rag
